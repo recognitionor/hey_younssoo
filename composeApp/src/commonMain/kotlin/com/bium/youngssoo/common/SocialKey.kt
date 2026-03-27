d208909d8a0cc284e3ea001827b77e951186bdd8
recognitionor <32305874+recognitionor@users.noreply.github.com>
2026-03-27 23:28:59 +0900

    init2

--- composeApp/src/commonMain/kotlin/com/bium/youngssoo/common/SocialKey.kt
+++
@@ -1,7 +0,0 @@
-package com.bium.youngssoo.common
-
-object SocialKey {
-    const val NAVER_CLIENT_ID = "t9ZJsIIvos3W2IqPRVWg"
-    const val NAVER_CLIENT_SECRET = "oD1SqxgLnE"
-    const val NAVER_CLIENT_NAME = "비움"
-}
